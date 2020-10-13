520cbc55daa65654334046900315ee625fd422cf
hyx3179 <hyx3179@gmail.com>
2020-10-13 21:40:11 +0800

    0.1

--- ACS.sh
+++ ACS.sh
@@ -5,11 +5,11 @@ PA=linux64
 wget https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_${PA}.tar.gz
 tar zxf subconverter_${PA}.tar.gz
 ./subconverter/subconverter -g
-git clone https://gist.github.com/${GITHUB_ACTOR}/${INPUT_REPOSITORY}.git
-cat ./subconverter/output.yml > ./${INPUT_REPOSITORY}/clash
-cd ./${INPUT_REPOSITORY}/
-git config --global user.email ${INPUT_EMAIL}
-git config --global user.name ${INPUT_NAME}
+git clone https://gist.github.com/${GITHUB_ACTOR}/${REPOSITORY}.git
+cat ./subconverter/output.yml > ./${REPOSITORY}/clash
+cd ./${REPOSITORY}/
+git config --global user.email ${EMAIL}
+git config --global user.name ${NAME}
 git commit -am 'update'
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@gist.github.com/${GITHUB_ACTOR}/${INPUT_REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@gist.github.com/${GITHUB_ACTOR}/${REPOSITORY}.git"
 git push "${remote_repo}"
